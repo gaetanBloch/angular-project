998d9ad07247e849ab0d756bd6a37e86b869539d
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-04 19:29:00 +0200

    Display Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'A Test Recipe',
       'This is simply a test',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
     )
   ];
 
