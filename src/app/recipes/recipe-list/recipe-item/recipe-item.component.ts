adefdbfa7bcaed2396f0d647d688d2c85e84afdb
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-11 22:13:54 +0200

    Passing dynamic parameter index to the route link to display a specific recipe

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,14 +9,8 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-
-  constructor(private recipeService: RecipeService) {
-  }
+  @Input() index: number;
 
   ngOnInit(): void {
-  }
-
-  onRecipeSelect() {
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 }
