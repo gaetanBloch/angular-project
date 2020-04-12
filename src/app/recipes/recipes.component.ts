9c3999286a02a94bc7f9e8c10523e1ababe06c25
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-12 11:50:43 +0200

    Removed useless Recipe EventEmitter

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -10,14 +8,9 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
-
-  constructor(private recipeService: RecipeService) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => this.selectedRecipe = recipe
-    );
   }
 }
