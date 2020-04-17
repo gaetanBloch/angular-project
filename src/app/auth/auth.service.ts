a3ec65390343fe3b54a3afcd3a738b037776fe2a
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-17 17:57:23 +0200

    Added Error handling on login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -16,6 +16,7 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
   readonly apiKey = 'AIzaSyAO-uajGQqcQ1p8YkIDu_QEj6ZAWlx6tuo';
+  readonly defaultErrorMessage = 'An Unknown error occurred!';
 
   constructor(private http: HttpClient) {
   }
@@ -24,35 +25,43 @@ export class AuthService {
     return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.apiKey,
       {email, password, returnSecureToken: true}
-    ).pipe(
-      catchError(errorRes => {
-        let errorMessage = 'An Unknown error occurred!';
-
-        if (!errorRes.error || !errorRes.error.error) {
-          return throwError(errorMessage);
-        }
-
-        switch (errorRes.error.error.message) {
-          case 'EMAIL_EXISTS' :
-            errorMessage = 'The email address is already in use by another account';
-            break;
-          case 'OPERATION_NOT_ALLOWED':
-            errorMessage = 'Password sign-in is disabled for this project';
-            break;
-          case 'TOO_MANY_ATTEMPTS_TRY_LATER' :
-            errorMessage = 'We have blocked all requests from this device due to ' +
-              'unusual activity. Try again later';
-            break;
-        }
-        return throwError(errorMessage);
-      })
-    );
+    ).pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string): Observable<AuthResponseData> {
     return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.apiKey,
       {email, password, returnSecureToken: true}
-    );
+    ).pipe(catchError(this.handleError));
+  }
+
+  handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = this.defaultErrorMessage;
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS' :
+        errorMessage = 'The email address is already in use by another account.';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password sign-in is disabled for this project.';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER' :
+        errorMessage = 'We have blocked all requests from this device due to ' +
+          'unusual activity. Try again later.';
+        break;
+      case 'EMAIL_NOT_FOUND' :
+        errorMessage = 'There is no user record corresponding to this identifier.' +
+          ' The user may have been deleted.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is invalid or the user does not have a password.';
+        break;
+      case 'USER_DISABLED' :
+        errorMessage = 'The user account has been disabled by an administrator.';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
