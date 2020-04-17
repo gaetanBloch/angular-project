679539d711deb8c648f3453a207a8d1e7c935875
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-17 10:10:21 +0200

    Use HTTP Put to simulate the Storage of all recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+
+  constructor(private dataStorageService: DataStorageService) {
+  }
+
+  onSaveData(): void {
+    this.dataStorageService.storeRecipes();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  }
+
+  storeRecipes(): void {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put<Recipe[]>(
+      'https://angular-project-11f6f.firebaseio.com/recipes.json',
+      recipes
+    ).subscribe(response => {
+      console.log(recipes);
+    });
   }
 }
