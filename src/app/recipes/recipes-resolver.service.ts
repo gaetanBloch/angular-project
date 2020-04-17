20bb5472a2de0d20a1e0fef6320faa552cb62051
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-17 11:15:10 +0200

    Fixed small bug to see immediate result of editing a recipe that was not reflected before

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -4,15 +4,23 @@ import { Observable } from 'rxjs';
 
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {
+  constructor(private dataStorageService: DataStorageService,
+              private recipeService: RecipeService) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();
+
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
 
 }
