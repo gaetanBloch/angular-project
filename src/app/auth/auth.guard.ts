094c02501cfc09f04c5fbedd57b7b8604481ad44
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-18 13:22:59 +0200

    Redirect to /auth when the user is not authenticated

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,17 +1,23 @@
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { AuthService } from './auth.service';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     Observable<boolean> | Promise<boolean> | boolean {
-    return this.authService.user.pipe(map(user => !!user));
+    return this.authService.user.pipe(
+      map(user => !!user),
+      tap(isAuthenticated => {
+        if (!isAuthenticated) {
+          this.router.navigate(['/auth']);
+        }
+      }));
   }
 }
