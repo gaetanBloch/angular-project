0162024b932eda132effb7c86e6dd05d949e25b0
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-18 14:36:03 +0200

    Close the Alert Component on click on the close button or the backdrop

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -21,6 +21,10 @@ export class AuthComponent {
     this.isLoginMode = !this.isLoginMode;
   }
 
+  onHandleError(): void {
+    this.error = null;
+  }
+
   onSubmit(form: NgForm): void {
     if (!form.valid) {
       return;
--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -7,4 +7,9 @@ import { Component, Input } from '@angular/core';
 })
 export class AlertComponent {
   @Input() message: string;
+  @Output() closeAlert = new EventEmitter<void>();
+
+  onClose(): void {
+    this.closeAlert.emit();
+  }
 }
