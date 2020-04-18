0bf2fc7f23f092a04ff9e7dd3d510663e5f77f92
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-18 14:53:48 +0200

    Created the Alert Component programmatically

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,10 @@
-import { Component, ComponentFactoryResolver } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from './auth.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
@@ -14,6 +15,7 @@ export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+  @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
 
   constructor(private authService: AuthService,
               private router: Router,
@@ -59,6 +61,8 @@ export class AuthComponent {
 
   private showErrorAlert(message: string): void {
     const alertFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
-
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+    hostViewContainerRef.createComponent(alertFactory);
   }
 }
