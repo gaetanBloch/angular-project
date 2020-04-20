9751cf6236d16b045565a39d964a7aa3b7af83d3
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-20 09:52:36 +0200

    Added the shoppingList Reduced with initial state

---
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,12 @@
+import { Ingredient } from '../shared/ingredient.model';
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+};
+
+export function ShoppingListReducer(state = initialState, action): void {
+
+}
