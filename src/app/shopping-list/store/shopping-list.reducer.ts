33a74a77ef51a7a0bd5471aaf317b7fda369008a
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-20 17:36:24 +0200

    Import everything from the ShoppingListActions

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,7 +1,5 @@
-import { Action } from '@ngrx/store';
-
 import { Ingredient } from '../../shared/ingredient.model';
-import { ADD_INGREDIENT } from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
   ingredients: [
@@ -10,12 +8,13 @@ const initialState = {
   ]
 };
 
-export function ShoppingListReducer(state = initialState, action: Action) {
+export function ShoppingListReducer(state = initialState,
+                                    action: ShoppingListActions.AddIngredient) {
   switch (action.type) {
-    case ADD_INGREDIENT:
+    case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingredients, action]
+        ingredients: [...state.ingredients, action.payload]
       };
   }
 }
