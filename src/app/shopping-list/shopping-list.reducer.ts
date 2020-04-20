d81a71d2a1e96b220470260f6ded9b4b1b656255
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-20 10:12:35 +0200

    Added logic to the ShoppingListReducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,3 +1,5 @@
+import { Action } from '@ngrx/store';
+
 import { Ingredient } from '../shared/ingredient.model';
 
 const initialState = {
@@ -7,6 +9,12 @@ const initialState = {
   ]
 };
 
-export function ShoppingListReducer(state = initialState, action): void {
-
+export function ShoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action]
+      };
+  }
 }
