9c41ceb80f7e58fb9e760c7424a0ca810324a81e
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-20 19:43:30 +0200

    Added the Add Ingredients action

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -9,12 +9,17 @@ const initialState = {
 };
 
 export function ShoppingListReducer(state = initialState,
-                                    action: ShoppingListActions.AddIngredient) {
+                                    action: ShoppingListActions.ShoppingListActions) {
   switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [...state.ingredients, action.payload]
+      };
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
       };
     default:
       return state;
