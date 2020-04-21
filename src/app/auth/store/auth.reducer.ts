517158bf2f0b68db05a602d8c091db34d7b8faac
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-21 16:32:16 +0200

    Handle the Login action in the Auth Reducer

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from '../user.model';
+import * as AuthActions from '../store/auth.actions';
 
 export interface State {
   user: User;
@@ -8,6 +9,20 @@ const initialState: State = {
   user: null
 };
 
-export function authReducer(state: State = initialState, action) {
-  return state;
+export function authReducer(state: State = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate
+      );
+      return {
+        ...state,
+        user
+      };
+    default:
+      return state;
+  }
 }
