200ab4beb249d2db2f3d113e5f956959dd682371
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-21 11:03:18 +0200

    Added StartEdit Action

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -6,6 +6,7 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
 export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
 export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+export const START_EDIT = 'START_EDIT';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
@@ -35,8 +36,16 @@ export class DeleteIngredient implements Action {
   }
 }
 
+export class StartEdit implements Action {
+  readonly type = START_EDIT;
+
+  constructor(public payload: number) {
+  }
+}
+
 export type ShoppingListActions =
   AddIngredient |
   AddIngredients |
   UpdateIngredient |
-  DeleteIngredient;
+  DeleteIngredient |
+  StartEdit;
