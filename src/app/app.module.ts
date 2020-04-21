80cc3d88dffdf0a3c23b0f2af33c06c6a2157172
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-21 15:56:11 +0200

    Import the auth reducer in the AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { ShoppingListReducer } from './shopping-list/store/shopping-list.reducer';
+import { authReducer } from './auth/store/auth.reducer';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,10 @@ import { ShoppingListReducer } from './shopping-list/store/shopping-list.reducer
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    StoreModule.forRoot({shoppingList: ShoppingListReducer}),
+    StoreModule.forRoot({
+      shoppingList: ShoppingListReducer,
+      auth: authReducer
+    }),
     SharedModule,
     CoreModule
   ],
