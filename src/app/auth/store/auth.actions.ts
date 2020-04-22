5709ff8861b28c72bf94af99e4bec2d325e1a92f
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-22 11:30:02 +0200

    Added the Login Fail Action

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -2,6 +2,7 @@ import { Action } from '@ngrx/store';
 
 export const LOGIN_START = '[Auth] Login Start';
 export const LOGIN = '[Auth] Login';
+export const LOGIN_FAIL = '[Auth] Login Fail';
 export const LOGOUT = '[Auth] Logout';
 
 export class LoginStart implements Action {
@@ -17,6 +18,13 @@ export class Login implements Action {
   constructor(public payload: {
     email: string, userId: string, token: string, expirationDate: Date
   }) {
+  }
+}
+
+export class LoginFail implements Action {
+  readonly type = LOGIN_FAIL;
+
+  constructor(public payload: string) {
   }
 }
 
