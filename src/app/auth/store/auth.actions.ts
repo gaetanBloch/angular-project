6580d0a0ec394c4c8498dc3f866a3edcc3ae8645
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-22 18:06:44 +0200

    Added Clear Error Action

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -4,6 +4,7 @@ export const LOGIN_START = '[Auth] Login Start';
 export const SIGN_UP_START = '[Auth] Sign Up Start';
 export const AUTHENTICATE_SUCCESS = '[Auth] Authenticate Success';
 export const AUTHENTICATE_FAIL = '[Auth] Authenticate Fail';
+export const CLEAR_ERROR = '[Auth] Clear Error';
 export const LOGOUT = '[Auth] Logout';
 
 export class LoginStart implements Action {
@@ -36,6 +37,11 @@ export class AuthenticateFail implements Action {
   }
 }
 
+export class ClearError implements Action {
+  readonly type = CLEAR_ERROR;
+}
+
+
 export class Logout implements Action {
   readonly type = LOGOUT;
 }
@@ -45,4 +51,5 @@ export type AuthActions =
   AuthenticateSuccess |
   AuthenticateFail |
   SignUpStart |
+  ClearError |
   Logout;
