61bea4938edec80649e1b520f1230814bcb27683
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-23 14:27:32 +0200

    Handle Update Recipe Action in the Reducer

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -22,6 +22,21 @@ export function recipeReducer(state: State = initialState, action: RecipeActions
         ...state,
         recipes: [...state.recipes, action.payload]
       };
+    case RecipeActions.UPDATE_RECIPE:
+      // Copy the recipe we want to update and merge the new recipe from the payload
+      const updatedRecipe = {
+        ...state.recipes[action.payload.index],
+        ...action.payload.recipe
+      };
+
+      // Copy the recipes and replace the updated recipe
+      const updatedRecipes = [...state.recipes];
+      updatedRecipes[action.payload.index] = updatedRecipe;
+
+      return {
+        ...state,
+        recipes: updatedRecipes
+      };
     default:
       return state;
   }
