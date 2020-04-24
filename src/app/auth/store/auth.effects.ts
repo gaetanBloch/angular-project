c7f48e4dd8bf77734533fc2781d0221a2ed529db
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-24 12:35:44 +0200

    Added new Auth SignUp effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError, map, switchMap, tap } from 'rxjs/operators';
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, Effect, ofType } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
 
@@ -11,6 +11,7 @@ import * as AuthActions from './auth.actions';
 import * as fromAuthActions from './auth.actions';
 import { User } from '../user.model';
 import { AuthService } from '../auth.service';
+import { Recipe } from '../../recipes/recipe.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -22,9 +23,98 @@ export interface AuthResponseData {
   registered?: boolean;
 }
 
+const handleAuthentication = (response: AuthResponseData): Action => {
+  const expiresInMillis = +response.expiresIn * 1000;
+  const expirationDate = new Date(new Date().getTime() + expiresInMillis);
+  const user = new User(response.email, response.localId, response.idToken, expirationDate);
+  localStorage.setItem('user', JSON.stringify(user));
+
+  return AuthActions.authenticateSuccess({
+    email: response.email,
+    userId: response.localId,
+    token: response.idToken,
+    expirationDate,
+    redirect: true
+  });
+}
+
+const handleError = (errorResponse: HttpErrorResponse): Observable<Action> => {
+  let errorMessage = 'An Unknown error occurred!';
+  if (!errorResponse.error || !errorResponse.error.error) {
+    return of(AuthActions.authenticateFail({errorMessage}));
+  }
+  switch (errorResponse.error.error.message) {
+    case 'EMAIL_EXISTS' :
+      errorMessage = 'The email address is already in use by another account.';
+      break;
+    case 'OPERATION_NOT_ALLOWED':
+      errorMessage = 'Password sign-in is disabled for this project.';
+      break;
+    case 'TOO_MANY_ATTEMPTS_TRY_LATER' :
+      errorMessage = 'We have blocked all requests from this device due to ' +
+        'unusual activity. Try again later.';
+      break;
+    case 'EMAIL_NOT_FOUND' :
+      errorMessage = 'There is no user record corresponding to this identifier.' +
+        ' The user may have been deleted.';
+      break;
+    case 'INVALID_PASSWORD':
+      errorMessage = 'The password is invalid or the user does not have a password.';
+      break;
+    case 'USER_DISABLED' :
+      errorMessage = 'The user account has been disabled by an administrator.';
+      break;
+  }
+  return of(AuthActions.authenticateFail({errorMessage}));
+}
+
 @Injectable()
 export class AuthEffects {
   static readonly API_KEY = environment.firebaseApiKey;
+
+  authSignUp$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.signUpStart),
+      switchMap(actionData => {
+        return this.http.post<AuthResponseData>(
+          'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+          AuthEffects.API_KEY,
+          {
+            email: actionData.email,
+            password: actionData.password,
+            returnSecureToken: true
+          }
+        ).pipe(
+          tap(this.setLogoutTimer.bind(this)),
+          map(handleAuthentication),
+          catchError(handleError)
+        )
+      })
+    )
+  );
+
+  fetchRecipes$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.signUpStart),
+      switchMap(() => {
+        return this.http
+          .get<Recipe[]>(
+            'https://shopping-app-6c625.firebaseio.com/recipes.json'
+          );
+      }),
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }),
+      map(recipes => {
+        return AuthActions.signUpStart({email: '', password: ''});
+      })
+    )
+  );
 
   @Effect()
   authSignUp = this.actions$.pipe(
@@ -126,49 +216,7 @@ export class AuthEffects {
               private authService: AuthService) {
   }
 
-  handleAuthentication(response: AuthResponseData): Action {
-    const expiresInMillis = +response.expiresIn * 1000;
-    const expirationDate = new Date(new Date().getTime() + expiresInMillis);
-    const user = new User(response.email, response.localId, response.idToken, expirationDate);
-    localStorage.setItem('user', JSON.stringify(user));
-    this.authService.setLogoutTimer(expiresInMillis);
-
-    return new AuthActions.AuthenticateSuccess({
-      email: response.email,
-      userId: response.localId,
-      token: response.idToken,
-      expirationDate,
-      redirect: true
-    });
-  }
-
-  handleError(errorResponse: HttpErrorResponse): Observable<Action> {
-    let errorMessage = 'An Unknown error occurred!';
-    if (!errorResponse.error || !errorResponse.error.error) {
-      return of(new AuthActions.AuthenticateFail(errorMessage));
-    }
-    switch (errorResponse.error.error.message) {
-      case 'EMAIL_EXISTS' :
-        errorMessage = 'The email address is already in use by another account.';
-        break;
-      case 'OPERATION_NOT_ALLOWED':
-        errorMessage = 'Password sign-in is disabled for this project.';
-        break;
-      case 'TOO_MANY_ATTEMPTS_TRY_LATER' :
-        errorMessage = 'We have blocked all requests from this device due to ' +
-          'unusual activity. Try again later.';
-        break;
-      case 'EMAIL_NOT_FOUND' :
-        errorMessage = 'There is no user record corresponding to this identifier.' +
-          ' The user may have been deleted.';
-        break;
-      case 'INVALID_PASSWORD':
-        errorMessage = 'The password is invalid or the user does not have a password.';
-        break;
-      case 'USER_DISABLED' :
-        errorMessage = 'The user account has been disabled by an administrator.';
-        break;
-    }
-    return of(new AuthActions.AuthenticateFail(errorMessage));
+  private setLogoutTimer(response: AuthResponseData) {
+    this.authService.setLogoutTimer(+response.expiresIn * 1000);
   }
 }
