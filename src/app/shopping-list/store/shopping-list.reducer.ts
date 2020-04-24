d2a4b598bf98c4a515fba64f6c2f500eb583716a
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-24 09:46:04 +0200

    Modified Ingredient Reducer State

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -4,14 +4,12 @@ import * as ShoppingListActions from './shopping-list.actions';
 
 export interface State {
   ingredients: Ingredient[];
-  editedIngredient: Ingredient;
-  editedIngredientIndex: number;
+  editIndex: number;
 }
 
 const initialState: State = {
   ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
-  editedIngredient: null,
-  editedIngredientIndex: -1
+  editIndex: -1
 };
 
 export function shoppingListReducer(state: State = initialState,
