1e09064e315738365608e10be57b60bb44d5b472
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-24 15:12:01 +0200

    Fixed the Recipes Resolver

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -10,24 +10,23 @@ import * as fromApp from '../store/app.reducer';
 import * as RecipeActions from '../recipes/store/recipe.actions';
 
 @Injectable({providedIn: 'root'})
-export class RecipesResolverService implements Resolve<Recipe[]> {
+export class RecipesResolverService implements Resolve<{ recipes: Recipe[] }> {
   constructor(private store: Store<fromApp.AppState>, private actions$: Actions) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
-    Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
+    Observable<{ recipes: Recipe[] }> | Promise<{ recipes: Recipe[] }> | { recipes: Recipe[] } {
     return this.store.select('recipes').pipe(
       take(1),
       map(recipesState => recipesState.recipes),
       switchMap(recipes => {
         if (recipes.length === 0) {
-          this.store.dispatch(new RecipeActions.FetchRecipes());
-          return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
+          this.store.dispatch(RecipeActions.fetchRecipes());
+          return this.actions$.pipe(ofType(RecipeActions.setRecipes), take(1));
         } else {
-          return of(recipes);
+          return of({recipes});
         }
       })
     )
-
   }
 }
