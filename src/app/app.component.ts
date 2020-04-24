9716cc19afd488465af5e72551dd267ddb78886d
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-24 15:45:53 +0200

    Check whether we are rendering the index page on the browser or the server to perform auto-login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Store } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
@@ -9,10 +10,15 @@ import * as AuthActions from './auth/store/auth.actions';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store<fromApp.AppState>) {
+  constructor(private store: Store<fromApp.AppState>,
+              @Inject(PLATFORM_ID) private platformId) {
   }
 
   ngOnInit(): void {
-    this.store.dispatch(AuthActions.autoLogin());
+    // Since auto-login relies on local storage we cannot use it when the page is rendered
+    // by the server, that's why we check here if we are on the Browser platform
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(AuthActions.autoLogin());
+    }
   }
 }
