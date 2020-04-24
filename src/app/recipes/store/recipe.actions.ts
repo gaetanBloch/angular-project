b6ccff62cfceae1bfa4297d2b3d7d293d5b9bbf5
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-24 14:39:44 +0200

    Create new Recipe Actions

--- src/app/recipes/store/recipe.actions.ts
+++ src/app/recipes/store/recipe.actions.ts
@@ -1,54 +1,26 @@
-import { Action } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 
 import { Recipe } from '../recipe.model';
 
-export const SET_RECIPES = '[Recipes] Set Recipes'
-export const FETCH_RECIPES = '[Recipes] Fetch Recipes'
-export const ADD_RECIPE = '[Recipes] Add Recipe'
-export const UPDATE_RECIPE = '[Recipes] Update Recipe'
-export const DELETE_RECIPE = '[Recipes] Delete Recipe'
-export const STORE_RECIPES = '[Recipes] Store Recipe'
-
-export class SetRecipes implements Action {
-  readonly type = SET_RECIPES;
-
-  constructor(public payload: Recipe[]) {
-  }
-}
-
-export class FetchRecipes implements Action {
-  readonly type = FETCH_RECIPES;
-}
-
-export class AddRecipe implements Action {
-  readonly type = ADD_RECIPE;
-
-  constructor(public payload: Recipe) {
-  }
-}
-
-export class UpdateRecipe implements Action {
-  readonly type = UPDATE_RECIPE;
-
-  constructor(public payload: { index: number, recipe: Recipe }) {
-  }
-}
-
-export class DeleteRecipe implements Action {
-  readonly type = DELETE_RECIPE;
-
-  constructor(public payload: number) {
-  }
-}
-
-export class StoreRecipes implements Action {
-  readonly type = STORE_RECIPES;
-}
-
-export type RecipeActions =
-  SetRecipes |
-  FetchRecipes |
-  AddRecipe |
-  UpdateRecipe |
-  DeleteRecipe |
-  StoreRecipes;
+export const setRecipes = createAction(
+  '[Recipes] Set Recipes',
+  props<{ recipes: Recipe[] }>()
+);
+export const fetchRecipes = createAction(
+  '[Recipes] Fetch Recipes'
+);
+export const addRecipe = createAction(
+  '[Recipes] Add Recipe',
+  props<{ recipe: Recipe }>()
+);
+export const updateRecipe = createAction(
+  '[Recipes] Update Recipe',
+  props<{ index: number, recipe: Recipe }>()
+);
+export const deleteRecipe = createAction(
+  '[Recipes] Delete Recipe',
+  props<{ index: number }>()
+);
+export const storeRecipes = createAction(
+  '[Recipes] Store Recipe'
+);
