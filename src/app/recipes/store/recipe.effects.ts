c28aeaa98947733fc83dd6d73818932e38158c9b
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-24 15:05:52 +0200

    Created new Recipe Effects

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, switchMap, withLatestFrom } from 'rxjs/operators';
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 
 import * as fromApp from '../../store/app.reducer';
@@ -11,38 +11,40 @@ import { Recipe } from '../recipe.model';
 @Injectable()
 export class RecipeEffects {
 
-  @Effect()
-  fetchRecipes = this.actions$.pipe(
-    ofType(RecipeActions.FETCH_RECIPES),
-    switchMap(() => {
-      return this.http.get<Recipe[]>(
-        'https://angular-project-11f6f.firebaseio.com/recipes.json'
-      ).pipe(
-        map(recipes => {
-          if (recipes) {
-            return recipes.map(recipe => {
-              // Set the ingredients to an empty array if it is not set
-              return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-            });
-          } else {
-            return [];
-          }
-        }),
-        map(recipes => new RecipeActions.SetRecipes(recipes))
-      )
-    })
+  fetchRecipes$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(RecipeActions.fetchRecipes),
+      switchMap(() => {
+        return this.http.get<Recipe[]>(
+          'https://angular-project-11f6f.firebaseio.com/recipes.json'
+        ).pipe(
+          map(recipes => {
+            if (recipes) {
+              return recipes.map(recipe => {
+                // Set the ingredients to an empty array if it is not set
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+              });
+            } else {
+              return [];
+            }
+          }),
+          map(recipes => RecipeActions.setRecipes({recipes}))
+        )
+      })
+    )
   );
 
-  @Effect({dispatch: false})
-  storeRecipes = this.actions$.pipe(
-    ofType(RecipeActions.STORE_RECIPES),
-    withLatestFrom(this.store.select('recipes')),
-    switchMap(([actionData, recipesState]) => {
-      return this.http.put<Recipe[]>(
-        'https://angular-project-11f6f.firebaseio.com/recipes.json',
-        recipesState.recipes
-      );
-    })
+  storeRecipes$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(RecipeActions.storeRecipes),
+      withLatestFrom(this.store.select('recipes')),
+      switchMap(([actionData, recipesState]) => {
+        return this.http.put<Recipe[]>(
+          'https://angular-project-11f6f.firebaseio.com/recipes.json',
+          recipesState.recipes
+        );
+      })
+    ), {dispatch: false}
   );
 
   constructor(private actions$: Actions,
